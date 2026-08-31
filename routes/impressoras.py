459f0e4c95410d63dd19d679c3d354c5d6b348c8
Renato <renatofranca2881@gmail.com>
2026-08-31 13:12:03 -0300

    Criada a rota de novas impressoras

--- routes/impressoras.py
+++ routes/impressoras.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from services.impressoras_services import ImpressorasService
 
 impressoras_bp = Blueprint(
@@ -15,3 +15,28 @@ def index():
         "impressoras/impressoras.html",
         impressoras=impressoras
     )
+    
+    
+@impressoras_bp.route("/nova", methods=["GET", "POST"])
+def nova_impressora():
+
+    if request.method == "POST":
+
+        nome = request.form.get("nome")
+        ip = request.form.get("ip")
+        modelo = request.form.get("modelo")
+
+        ImpressorasService.adicionar_impressora(
+            nome,
+            ip,
+            modelo
+        )
+
+        return redirect(
+            url_for("impressoras.index")
+        )
+
+    return render_template(
+        "impressoras/nova_impressora.html"
+    )
+    
--- services/impressoras_services.py
+++ services/impressoras_services.py
@@ -1,10 +1,32 @@
-from models import Impressora
+from models import Impressora, db
 
 class ImpressorasService:
 
     @staticmethod
     def listar_impressoras():
         return Impressora.query.all()
+
+    @staticmethod
+    def adicionar_impressora(nome, ip, modelo):
+        existente = Impressora.query.filter_by(
+                    ip=ip
+                ).first()
+                
+        if existente:
+            return existente
+        
+        impressora = Impressora(
+            nome=nome,
+            ip=ip,
+            modelo=modelo
+        )
+        
+        
+        db.session.add(impressora)
+        db.session.commit()
+        
+        return impressora
+        
 
     @staticmethod
     def descobrir_impressoras():
